6e8691ba3b4a6567968b7a53e6370edda4bad725
Tomas Jansson <tomas.jansson@gmail.com>
2013-11-21 19:41:52 +0100

    bare bone simple.web console with fix and nowin

---
+++ NestDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestDemo
+{
+    using Fix;
+    using Nowin;
+    using Simple.Web;
+
+    class Program
+    {
+        const int Port = 1337;
+
+        public static Type[] EnforceReferencesFor =
+        {
+            typeof (Simple.Web.Razor.RazorHtmlMediaTypeHandler),
+            typeof (Simple.Web.JsonNet.JsonMediaTypeHandler)
+        };
+
+        static void Main(string[] args)
+        {
+            var app = new Fixer()
+                .Use(Application.Run)
+                .Build();
+
+            var serverBuilder = ServerBuilder.New()
+                .SetPort(Port)
+                .SetOwinApp(app);
+
+            using (serverBuilder.Start())
+            {
+                Console.WriteLine("Listening on port {0}, press enter to exit", Port);
+                Console.ReadLine();
+            }
+        }
+    }
+}
